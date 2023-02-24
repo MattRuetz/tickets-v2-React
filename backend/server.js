352d0717c6761254ad7b0ba28af1b9643bc7a923
MattRuetz <matt.b.ruetz@gmail.com>
2023-02-23 19:48:34 -0500

    last commit before heroku push

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 // dotenv NPM package
@@ -18,12 +19,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello!' });
+    res.status(200).json({ message: 'Support Desk API' });
 });
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Support Desk API' });
+    });
+}
 
 app.use(errorHandler);
 
